8ea9840bb3513c8355f9bdceb4df0b9b1e85fcc3
jakenester31 <111662560+jakenester31@users.noreply.github.com>
2025-04-13 14:55:53 -0700

    Add files via upload

--- workspace/chart.js
+++ workspace/chart.js
@@ -1,33 +1,27 @@
-var workspace = document.getElementById("workspace");
-const sbh = document.getElementById("sbh");
-const body = document.getElementsByTagName("body")[0];
-var mx;
 var sbh_md;
 
-
-sbh.addEventListener("mousedown", (event) => {   //Mouse Down?
+sbh.addEventListener("mousedown", function(){   //Mouse Down?
     sbh_md = 1;
 });
 
-addEventListener("mouseup", (event) =>{ //Mouse up?
+addEventListener("mouseup", function(){ //Mouse up?
     sbh_md=0;
 });
 
-addEventListener('mouseleave',(event) => { //Mouse leave document?
+addEventListener('mouseleave', function(){ //Mouse leave document?
     sbh_md = 0;
 });
 
 addEventListener('mousemove',(event) => { //Mouse Move?
-    mx = event.clientX;
-    if (sbh_md == 1){
-        let x = mx
-        mx < 50 && (x = 50); //min size
-        mx > 200 && (x = 200); //max size
-        sbh.style.left= x - 5 + "px";
-        document.getElementById("sidebar").style.width=x + "px";
-        document.getElementById("w_container").style.left= x + "px";
-        document.getElementById("sidebar").style.backgroundColor="skyblue";
-    } else {
-        document.getElementById("sidebar").style.backgroundColor="white";
+    if (sbh_md == 1){ // Mouse is down and moving
+        let mx = event.clientX;
+        mx < 50 && (mx = 50); //min size
+        mx > 200 && (mx = 200); //max size
+        document.getElementById("sbh").style.left= mx - 5 + "px";
+        document.getElementById("sidebar").style.width= mx + "px";
+        document.getElementById("w_container").style.left= mx + "px";
+        document.getElementById("sidebar").style.backgroundColor= "skyblue";
+    } else { // mouse is not moving
+        document.getElementById("sidebar").style.backgroundColor= "white";
     }
 });
--- workspace/workspace.js
+++ workspace/workspace.js
@@ -1,76 +1,168 @@
 //Setup workspace
-var context = workspace.getContext("2d");
+var canvas = document.getElementById("workspace");
+var context = canvas.getContext("2d");
 var mp = [[0,0],[0,0]];
-setInterval(draw,5);
-//General
-var Workspace = {x:0,y:0,md:0,scale:1};
-var mouse = {x:0,y:0,ox:0,oy:0,gx:0,gy:0};
+setInterval(draw,50);
+//General Info
+var workspace = {x:0,y:0,md:0,scale:1};
+var mouse = {gx:0,gy:0,down:0};
+var objects = [];
 
 // Fixes the aspect ratio
-new ResizeObserver(resize).observe(workspace);
+new ResizeObserver(resize).observe(canvas);
 function resize() {
-    workspace.height = workspace.clientHeight * Workspace.scale;
-    workspace.width = workspace.height * (workspace.clientWidth / workspace.clientHeight);
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
     draw();
 }
 
-// When the mouse is moving over workspace
-workspace.addEventListener('mousemove',(event) => {
-    // Get mouse positions
-    mp.splice(0,1); //removes the old old item
-    mp.push([
-        event.clientX - document.getElementById('sidebar').clientWidth,
-        event.clientY - document.getElementById('head').clientHeight
-    ]);
-    // TEST SCRIPT, [DELETE ME]
-    mouse.gx= mouse.x * Workspace.scale - Workspace.x;
-    mouse.gy= mouse.y * Workspace.scale - Workspace.y;
-    mouse = {ox:mp[0][0],oy:mp[0][1],x:mp[1][0],y:mp[1][1],gx:mouse.gx,gy:mouse.gy};
+// Mouse events:
 
-    // Reposition workspace
-    if (Workspace.md == 1){
-        Workspace.x += (mouse.x - mouse.ox) * Workspace.scale;
-        Workspace.y += (mouse.y - mouse.oy) * Workspace.scale;
+// mouse moving over canvas
+canvas.addEventListener('mousemove',(event) => {
+    // Get mouse positions
+    mp.splice(0,1); //removes the old item
+    mp.push([ //adds new items
+        event.clientX - document.getElementById('sidebar').clientWidth, //mouse x
+        event.clientY - document.getElementById('head').clientHeight //mouse y
+    ]);
+    if (mouse.down == 1){ // mouse down? move workspace
+        workspace.x += (mp[1][0] - mp[0][0]);
+        workspace.y += (mp[1][1] - mp[0][1]);
     }
+    Object.assign(mouse,{gx:(mp[1][0] - workspace.x) / workspace.scale,gy:(mp[1][1]- workspace.y) / workspace.scale}); //assigns mouse info
 });
 
 // mouse down and up
-workspace.addEventListener('mousedown', (event) => {
-    Workspace.md = 1;
+canvas.addEventListener('mousedown', (event) => {
+    mouse.down = 1;
 });
 
 addEventListener('mouseup', (event) => {
-    Workspace.md = 0;
+    mouse.down = 0;
 });
-
 
 //Scrolling
 onwheel = (event) => {
-    let oldscale = Workspace.scale;
-    event.deltaY < 0 && (Workspace.scale -= Workspace.scale / 10);
-    event.deltaY > 0 && (Workspace.scale += Workspace.scale / 10);
-    Workspace.x += mouse.x * (Workspace.scale - oldscale);
-    Workspace.y += mouse.y * (Workspace.scale - oldscale);
+    let old = workspace.scale;
+    event.deltaY < 0 && (workspace.scale += workspace.scale / 10);
+    event.deltaY > 0 && (workspace.scale -= workspace.scale / 10);
+    workspace.scale < 0.1 && (workspace.scale = 0.1);
+    workspace.scale > 7 && (workspace.scale = 7);
+    workspace.x -= mouse.gx * (workspace.scale - old);
+    workspace.y -= mouse.gy * (workspace.scale - old);
     resize();
 };
 
 
 
-function draw () {
-    context.clearRect(0,0,workspace.width,workspace.height);
-    context.fillRect(Workspace.x,Workspace.y,100,100);
-    // 0,0 to mouse
-    context.beginPath();
-        context.moveTo(...goto(0,0));
-        context.lineTo(mouse.x * Workspace.scale, mouse.y * Workspace.scale);
-    context.stroke();
-    // 0,0 to 200,100
-    context.beginPath();
-        context.moveTo(...goto(0,0));
-        context.lineTo(200 + Workspace.x,100 + Workspace.y);
-    context.stroke();
+
+
+
+// converts graph position to canvas position
+function goto(x,y){
+    return([workspace.x + x * workspace.scale, workspace.y + y * workspace.scale])
 }
 
-function goto(x,y){
-    return([Workspace.x + x, Workspace.y + y])
+function size(w,h){
+    return([w * workspace.scale, h * workspace.scale]);
 }
+
+// DIVORCE!!!!
+function divorce (x){
+    let string = JSON.stringify(x);
+    let result = "";
+    let bad = [",",'"',"{","}","[","]"];
+    let ob = 0;
+    for (var i = 0; i < string.length; i++){
+        let char = string[i];
+        if (bad.indexOf(char) == -1){
+            result += char;
+        } else if (bad.indexOf(char) == 0 || bad.indexOf(char) == 2) {
+            if (bad.indexOf(char) == 2) {
+                // result += "NEW_OBJECT";
+                // result += Object.entries(x)[1][0] + "___123123123123123";
+                if (ob > 2){
+                    result += "<br>____" + Object.entries(x)[2][1][ob - 3].constructor.name + ":";
+                }
+                ob += 1;
+            }
+            result += "<br>";
+        }
+    }
+    return(result);
+}
+
+// Draws the canvas
+function draw () {
+    document.getElementById('sidebar').innerHTML= divorce({mouse,workspace,objects}); // [DELETE ME] debug info
+    // Reset canvas
+    context.lineWidth=3 * workspace.scale;
+    context.strokeStyle="black";
+    context.clearRect(0,0,canvas.width,canvas.height); //Reset canvas
+    for (var i = 0; i < objects.length; i++){
+        objects[i].draw();
+    }
+    m.x2 = mouse.gx;
+    m.y2 = mouse.gy;
+}
+
+class circle {
+    constructor(x,y,radius,color){
+        Object.assign(this,{x:x,y:y,radius:radius,color:color})
+        objects.push(this);
+    }
+    draw(){
+        context.strokeStyle=this.color;
+        context.beginPath();
+            context.arc(...goto(this.x,this.y),this.radius * workspace.scale,0,Math.PI * 2);
+        context.closePath;
+        context.stroke();
+    }
+}
+
+class line {
+    constructor(x,y,x2,y2,color,width){
+        Object.assign(this,{x:x,y:y,x2:x2,y2:y2,color:color,width:width})
+        objects.push(this);
+    }
+    draw(){
+        context.strokeStyle=this.color;
+        context.lineWidth=this.width * workspace.scale;
+        context.beginPath();
+            context.moveTo(...goto(this.x,this.y));
+            context.lineTo(...goto(this.x2, this.y2));
+        context.closePath();
+        context.stroke();
+    }
+}
+
+class rect {
+    constructor(x,y,width,height,color){
+        Object.assign(this,{x:x,y:y,width:width,height:height,color:color})
+        objects.push(this);
+    }
+    draw(){
+    context.fillStyle = this.color;
+    context.fillRect(...goto(this.x,this.y),...size(this.width,this.height));
+    }
+}
+
+class text {
+    constructor(x,y,txt,size,maxWidth){
+        Object.assign(this,{x:x,y:y,text:txt,size:size,maxWidth:maxWidth});
+        objects.push(this);
+    }
+    draw(){
+        context.font= this.size * workspace.scale + "px Ariel";
+        context.fillText(this.text,...goto(this.x,this.y),this.maxWidth);
+    }
+}
+
+
+
+let c1 = new circle(450,450,50,"black");
+let l1 = new line(200,0,0,200,"purple",5);
+let rectangle = new rect(0,0,100,100,"red");
+let txt1 = new text(0,300,"BALLER",100)
+let m = new line(0,0,0,0,"black");
