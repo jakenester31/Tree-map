057d676e500d32af8561ff2bbe4b890e8cd20d3b
jakenester31 <111662560+jakenester31@users.noreply.github.com>
2025-05-04 18:56:16 -0700

    Massive update
    Im too lazy to describe everything that updated, just cross reference this version and the one before ok!

--- workspace/classes.js
+++ workspace/classes.js
@@ -1,27 +1,4 @@
-class rect {
-    constructor (x,y,width,height) {
-        Object.assign(this,{x:x,y:y,width:width,height:height})
-        objects.push(this);
-    }
-    draw(){
-        context.fillRect(...goto(this.x,this.y),...size(this.width,this.height));
-    }
-}
-
-class line {
-    constructor (x,y,x2,y2) {
-        Object.assign(this,{x:x,y:y,x2:x2,y2:y2});
-        objects.push(this);
-    }
-    draw(){
-        context.beginPath();
-            context.moveTo(...goto(this.x,this.y));
-            context.lineTo(...goto(this.x2, this.y2));
-        context.closePath();
-        context.stroke();
-    }
-}
-
+// Settings
 class lineWidth {
     constructor (width) {
         this.width = width;
@@ -40,5 +17,115 @@ class color {
     draw (){
         context.strokeStyle = this.color;
         context.fillStyle = this.color;
+    }
+}
+
+// Objects
+class rect {
+    constructor (x,y,width,height) {
+        Object.assign(this,{x:x,y:y,width:width,height:height})
+        this.hover = 0;
+        objects.push(this);
+    }
+    draw(){
+        context.fillRect(...goto(this.x,this.y),...size(this.width,this.height));
+        
+        if (this.collider != 1){
+            return(0);
+        }
+        this.hover = 0;
+
+        if (workspace.md != 1) {
+            return(0);
+        }
+        if (!(gPos(mouse[0]).x > this.x && gPos(mouse[0]).x < this.x + this.width)){
+            return(0);
+        }
+        if (gPos(mouse[1]).y > this.y && gPos(mouse[1]).y < this.y + this.height){
+            this.hover = 1;
+            over.push(this);
+        }
+    }
+    outline(){
+        const save = context.lineWidth
+        context.lineWidth = size(5);
+        context.strokeStyle='lime';
+        context.beginPath();
+            context.moveTo(...goto(this.x,this.y));
+            context.lineTo(...goto(this.x + this.width, this.y));
+            context.lineTo(...goto(this.x + this.width, this.y + this.height));
+            context.lineTo(...goto(this.x, this.y + this.height));
+            context.lineTo(...goto(this.x, this.y));
+            context.stroke();
+        context.closePath();
+        context.lineWidth = size(save);
+    }
+    addCollider () {
+        this.collider = 1;
+    }
+}
+
+class line {
+    constructor (x,y,x2,y2) {
+        Object.assign(this,{x:x,y:y,x2:x2,y2:y2});
+        objects.push(this);
+    }
+    draw(){
+        context.beginPath();
+            context.moveTo(...goto(this.x,this.y));
+            context.lineTo(...goto(this.x2, this.y2));
+            context.stroke();
+        context.closePath();
+    }
+}
+
+class curve {
+    constructor(point1,point2,controler1,controler2){
+        Object.assign(this,{point1:point1,point2:point2,controler1:controler1,controler2:controler2});
+        objects.push(this);
+        if (controler1 == 'mid' || controler1 == undefined) {
+            this.controler1 = [(point1[0] + point2[0]) / 2,point1[1]]
+        }
+        if (controler2 == 'mid' || controler2 == undefined) {
+            this.controler2 = [(point1[0] + point2[0]) / 2,point2[1]]
+        }
+    }
+    draw(){
+        context.beginPath();
+            context.moveTo(...goto(...this.point1));
+            context.bezierCurveTo(...goto(...this.controler1),...goto(...this.controler2),...goto(...this.point2));
+            context.stroke();
+        context.closePath();
+    }
+}
+
+
+class arc {
+    constructor(x,y,radius,fill){
+        Object.assign(this,{x:x,y:y,radius:radius,fill,fill})
+        objects.push(this);
+    }
+    draw(){
+        context.beginPath();
+            context.arc(...goto(this.x,this.y),size(this.radius),Math.PI * -arcAngle[1], Math.PI * -arcAngle[0]);
+            this.fill == 1 && context.fill();
+            context.stroke();
+        context.closePath();
+    }
+}
+
+class setArc {
+    constructor(angle1,angle2){
+        Object.assign(this,{angle1:angle1,angle2:angle2})
+        objects.push(this);
+    }
+    draw(){
+        arcAngle = [this.angle1,this.angle2];
+        if (Math.abs(this.angle1) > 2) {
+            this.angle1 = 2 * (this.angle1 / Math.abs(this.angle1));
+        }
+        if (Math.abs(this.angle2) > 2) {
+            this.angle2 = 2 * (this.angle2 / Math.abs(this.angle2));
+        }
     }
 }
--- workspace/workspace.js
+++ workspace/workspace.js
@@ -1,8 +1,10 @@
 // General
 const canvas = document.querySelector('#workspace');
 const context = canvas.getContext("2d");
-const workspace = {x:0,y:0,scale:1};
+const workspace = {x:0,y:0,scale:1,md:0};
 const objects = [];
+var mouse = [0,0];
+var target;
 
 new ResizeObserver(resizeCanvas).observe(canvas);
 function resizeCanvas() {
@@ -37,63 +39,121 @@ canvas.addEventListener('mousedown', e =>{
     if (e.button !== 0){
         return(0);
     }
-    const mouse = [[e.clientX,e.clientY],[e.clientX,e.clientY]];
+    const mousePos = [];
+    while (mousePos.length < 2) {
+        mousePos.push([e.clientX - canvas.parentNode.offsetLeft,e.clientY - canvas.parentNode.offsetTop])
+    }
+    mouse = [
+        mousePos[1][0] + canvas.parentNode.offsetLeft,
+        mousePos[1][1] + canvas.parentNode.offsetTop
+    ];
     const move = function (e){
-        mouse.splice(0,1);
-        mouse.push([e.clientX,e.clientY])
-    
-        workspace.x += (mouse[1][0] - mouse[0][0]);
-        workspace.y += (mouse[1][1] - mouse[0][1]);
+        // mousePos positions
+        mousePos.splice(0,1);
+        mousePos.push([e.clientX - canvas.parentNode.offsetLeft,e.clientY - canvas.parentNode.offsetTop])
+        // mousePos bounding
+        for (var i = 0; i < 2; i++){
+            mousePos[1][i] < 0 && (mousePos[1][i] = 0);
+        }
+        mousePos[1][0] > canvas.clientWidth && (mousePos[1][0] = mousePos[0][0]);
+        mousePos[1][1] > canvas.clientHeight && (mousePos[1][1] = mousePos[0][1]);
+        // Record mouse as global
+        mouse = [
+            mousePos[1][0] + canvas.parentNode.offsetLeft,
+            mousePos[1][1] + canvas.parentNode.offsetTop
+        ];
+        // reposition target
+        if (target !== undefined){
+            // console.log(
+            //     (mousePos[1][0] - mousePos[0][0]) / workspace.scale,
+            //     (mousePos[1][1] - mousePos[0][1]) / workspace.scale
+            // );
+            target.x += (mousePos[1][0] - mousePos[0][0]) / workspace.scale;
+            target.y += (mousePos[1][1] - mousePos[0][1]) / workspace.scale;
+            return(0);
+        }
+        // reposition workspace
+        workspace.x += (mousePos[1][0] - mousePos[0][0]);
+        workspace.y += (mousePos[1][1] - mousePos[0][1]);
     }
     const stop = function(){
+        workspace.md = 0;
+        target = undefined;
+        console.log('Killed');
         removeEventListener('mousemove',move);
         removeEventListener('mouseup',stop);
-        console.log('killed');
     }
 
     addEventListener('mousemove', move)
     addEventListener('mouseup', stop)
-    console.log('born');
+    workspace.md = 1;
+    target = undefined;
 })
 
 canvas.addEventListener('wheel', e=>{
     const old = workspace.scale;
-    const mp = {x:gPos(e.clientX).x,y:gPos(e.clientY).y};
-
+    // which position?
+    let mp = {};
+    if (workspace.md == 1) {
+        mp = {x:gPos(mouse[0]).x,y:gPos(mouse[1]).y};
+    } else {
+        mp = {x:gPos(e.clientX).x,y:gPos(e.clientY).y};
+    }
+    // new scale
     e.deltaY < 0 && (workspace.scale += workspace.scale / 10);
     e.deltaY > 0 && (workspace.scale -= workspace.scale / 10);
-
+    // bound scale
     workspace.scale < 0.1 && (workspace.scale = 0.1);
     workspace.scale > 7 && (workspace.scale = 7);
-
+    // offset workspace
     workspace.x -= mp.x * (workspace.scale - old);
     workspace.y -= mp.y * (workspace.scale - old);
 });
 
-// Classes
 
+function addCollider (obj) {
+    obj.collider = 1;
+}
+
+
+var arcAngle = [0,2];
+
+// Classes
 new rect(-1,-1,1,1);
 new color('red');
-new rect(0,0,100,100);
-new rect(100,100,100,100);
+new rect(0,0,100,100).addCollider();
+new rect(100,100,100,100).addCollider();
 new lineWidth(7);
 new color('skyblue');
 new line(200,200,300,300);
+new line(310,310,330,330)
 new rect(300,200,1000,100);
+new color('purple');
+new lineWidth(3);
+new curve([200,0],[400,100]);
+new setArc(0.2,1.2);
+new arc(500,100,50);
 
 setInterval(draw, 20);
-
-
+var over = [];
 function draw(){
     // setup
     context.lineWidth = size(3);
     context.strokeStyle='black';
     context.fillStyle = 'black';
     context.clearRect(0,0,canvas.width,canvas.height);
+
     //draw objects
+    over = [];
+
     for (var i = 0; i < objects.length; i++){
+        // target !== undefined && target === objects[i] && target.outline();
         objects[i].draw();
     }
+    if (target === undefined){
+        target = over[over.length - 1];
+    }
+    target !== undefined && target.outline();
 
-    // document.querySelector('#sidebar').innerHTML=test.join('<br>');
+    document.querySelector('#sidebar').innerHTML=[workspace.md].join('<br>');
 }
